df21f1b4b2e99a7d302e46aea5c63379606bc463
Tustoz <maxiadityaksm@gmail.com>
2023-08-09 23:36:08 +0700

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,175 @@
+export const info = {
+  baseUrl: "https://tustoz-portfolio.vercel.app",
+  name: "Maxi Aditya Kusuma W",
+  jobDescription: "Flutter Developer",
+  about: `I'm a software engineer based in Jakarta, Indonesia.
+
+   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
+   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Mobile App Development.
+   I have 3 years of experience in Dart, JavaScript, Flutter, ReactJS, React Native, Node JS, Express JS, Firebase, Git.
+   I have successfully completed several mobile app projects from scratch.
+   Yet, I learn continuously and never stop exploring new technologies.
+   So, I have also gained knowledge of HTML, CSS, SQL, MongoDB, GraphQL, and Python
+   with time.
+  `,
+
+  experience: [
+    {
+      name: "Freelance Flutter Developer @ Fiverr International, Ltd",
+      location: "Jakarta, Special Capital Region of Jakarta, Indonesia",
+      startDate: "Jan 2023",
+      endDate: "Present",
+      description: [
+        "✓ Provide solutions to client needs, from user interface design to development of unique features.",
+        "✓ Ensuring the developed applications meet quality standards and client needs through unit tests.",
+        "✓ Develop mobile-based applications that can run on the Android and iOS platforms and upload them to the appstore and playstore.",
+      ],
+    },
+
+    // {
+    //   name: "Flutter Developer @ The Shalom and Levy Group, Inc",
+    //   location: "Beverly Hills, California, United States",
+    //   startDate: "Jun 2023",
+    //   endDate: "Aug 2023",
+    //   description: [
+    //     "The Shalom and Levy Group is a wellness company that is focused on mental wellness for everyone. Their app 'Boom Journal' is the one-stop shop for all tools related to mental well-being. They offer a daily guided mindful journal, as well as other resources to help people improve their mental health.",
+    //     "‎",
+    //     "Some of my responsibilities:",
+    //     "‎",
+    //     "✓ Redesign existing Boom Journal applications to enhance functionality.",
+    //     "✓ Develop and maintain Flutter applications for Boom Journal, ensuring efficient user experience.",
+    //     "✓ Write clean, well-structured, and maintainable code using Dart and Flutter framework.",
+    //     "✓ Conduct thorough testing of the applications to identify and fix bugs, performance issues, and compatibility problems across various devices.",
+    //     "✓ Integrate Firebase backend services (such as Firestore, Authentication, Cloud Functions, etc.) to handle data storage, user authentication, in-app chat, and serverless functions.",
+    //   ],
+    // },
+
+    {
+      name: "Data Administrator @ Linfox Logistic Indonesia, Ltd",
+      location: "Bekasi, West Java, Indonesia",
+      startDate: "Nov 2020",
+      endDate: "Dec 2022",
+      description: [
+        "Linfox is a global supply chain and logistics company with operations in Australia, New Zealand, Asia, Europe, and the Middle East. In Indonesia, Linfox provides a range of services, including: Warehousing and distribution, Transport and freight management, Analytical and advisory services, Technology solutions.",
+        "‎",
+        "Some of my responsibilities:",
+        "‎",
+        "✓ Maintains records of all imaging activities.",
+        "✓ Reviews scanned images for quality and accuracy.",
+        "✓ Monitors and maintains the imaging equipment to ensure proper operation and quality of images.",
+        "✓ Monitors and operates the program console in accordance with operating instructions to initiate and control the imaging process.",
+      ],
+    },
+
+    {
+      name: "Co-Founder @ Muju Indonesia Maju, Ltd",
+      location: "South Tangerang, Banten, Indonesia",
+      startDate: "Dec 2020",
+      endDate: "Jul 2022",
+      description: [
+        "Muju is a platform that lets you book sports venues and fields quickly and easily.",
+        "‎",
+        "Some of my responsibilities:",
+        "‎",
+        "✓ Write high-quality, clean, and maintainable code and technical documentation.",
+        "✓ Actively collaborate with cross-functional teams to define, design, and ship new features.",
+        "✓ Maintaining software through product lifecycle including design, development, verification and bug fixes.",
+        "✓ Design, develop, and maintain high-quality mobile applications for user & partner using the Flutter framework.",
+      ],
+    },
+
+    {
+      name: "Tech Influencer @ TikTok, Ltd",
+      location: "Jakarta, Special Capital Region of Jakarta, Indonesia",
+      startDate: "Mar 2020",
+      endDate: "May 2022",
+      description: [
+        "✓ Crafting engaging and informative scripts for coding-related videos. These scripts outline the content, structure, and key points to be covered in the video.",
+        "✓ Answering questions and Explaining basic coding concepts to followers.",
+        "✓ Reviewing and recommending tools and learning resources for coding.",
+        "✓ Collaborating with technology brands, both nationally and internationally, such as: Logitech, Niagahoster, Progate, Kotakode, AI Archives, Loop CV, Reimagine Home, Blackbox AI, DeepBrain AI, Sharetings, Camp404, Emurgo.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Calvin Institute of Technology (CIT)",
+      location: "Central Jakarta, Special Capital Region of Jakarta, Indonesia",
+      startDate: "2022",
+      endDate: "Present",
+      description: [
+        "(Undergraduate) Bachelor of Informatics Engineering, IT & Big Data Analytics (IBDA), GPA: 3.0",
+      ],
+    },
+    {
+      name: "Filiasofia Senior High School",
+      location: "Pekanbaru, Riau, Indonesia",
+      startDate: "2018",
+      endDate: "2022",
+      description: ["Natural Sciences Major, GPA: 82.17"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/tustozy",
+    instagram: "https://instagram.com/maxi.aditya",
+    github: "https://github.com/tustoz",
+    email: "maxiadityaksm@gmail.com",
+    linkedin: "https://www.linkedin.com/in/maxiaditya/",
+  },
+
+  projects: [
+    // {
+    //   title: "Muju User App",
+    //   isFeatured: true,
+    //   thumbnail: "/assets/images/astro-portfolio.png",
+    //   githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+    // },
+
+    {
+      title: "Car Rental App",
+      isFeatured: true,
+      thumbnail: "/assets/images/car-rental.png",
+      githubUrl: "https://github.com/tustoz/car_rental",
+    },
+
+    {
+      title: "Crypto Wallet App",
+      isFeatured: false,
+      thumbnail: "/assets/images/crypto-wallet.png",
+      githubUrl: "https://github.com/tustoz/crypto_wallet",
+    },
+
+    {
+      title: "Online Course App",
+      isFeatured: false,
+      thumbnail: "/assets/images/online-course.jpg",
+      githubUrl: "https://github.com/tustoz/online_course",
+    },
+
+
+
+    {
+      title: "Netflix Redesign App",
+      isFeatured: true,
+      thumbnail: "/assets/images/netflix-redesign.png",
+      githubUrl: "https://github.com/tustoz/netflix_redesign",
+      // liveUrl: "https://github.com/tustoz/netflix_redesign/releases/download/0.0.1/app-release.apk",
+    },
+
+    {
+      title: "Coffeeholic App",
+      isFeatured: false,
+      thumbnail: "/assets/images/coffee-shop-finder.png",
+      githubUrl: "https://github.com/tustoz/coffee_shop_finder",
+    },
+
+    {
+      title: "Foodies App",
+      isFeatured: false,
+      thumbnail: "/assets/images/foodies.png",
+      githubUrl: "https://github.com/tustoz/foodies",
+    },
+  ],
+};
