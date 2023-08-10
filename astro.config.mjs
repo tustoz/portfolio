4e46049c0f5be27bd84771b2a24ebb03b2d01678
Tustoz <maxiadityaksm@gmail.com>
2023-08-10 12:15:54 +0700

    added astro-compressor package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,20 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
+import compressor from "astro-compressor";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tustoz-portfolio.vercel.app",
-  integrations: [
-    tailwind(),
-    sitemap({
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date(),
-      entryLimit: 10000,
-    }),
-    robotsTxt(),
-  ],
+  integrations: [tailwind(), sitemap({
+    changefreq: "weekly",
+    priority: 0.7,
+    lastmod: new Date(),
+    entryLimit: 10000
+  }), robotsTxt(), compressor({
+    gzip: true,
+    brotli: true
+  })]
 });
